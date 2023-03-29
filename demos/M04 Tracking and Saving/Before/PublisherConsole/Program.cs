ff0f06394f46d1651dee27579f60faaf3ddd43e1
antonio-dembo <beboantonio1@hotmail.com>
2023-03-29 16:10:20 -0300

    insert more data

--- demos/M04 Tracking and Saving/Before/PublisherConsole/Program.cs
+++ demos/M04 Tracking and Saving/Before/PublisherConsole/Program.cs
@@ -3,9 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using PublisherData;
 using PublisherDomain;
 
-
 PubContext _context = new PubContext();
 //this assumes you are working with the populated
 //database created in previous module
 
-   
+InsertAuthor();
+
+void InsertAuthor()
+{
+    //var author = new Author { FirstName= "Frank", LastName = "Herbert" };
+
+    _context.Authors.Add(new Author { FirstName = "Dan", LastName = "Apple" });
+    _context.Authors.Add(new Author { FirstName = "Ruth", LastName = "Ozeki" });
+    _context.Authors.Add(new Author { FirstName = "Sofia", LastName = "Segovia" });
+    _context.Authors.Add(new Author { FirstName = "Ursula K.", LastName = "LeGuin" });
+    _context.Authors.Add(new Author { FirstName = "Hugh", LastName = "Howey" });
+    _context.Authors.Add(new Author { FirstName = "Isabelle", LastName = "Allende" });
+    _context.SaveChanges();
+}
