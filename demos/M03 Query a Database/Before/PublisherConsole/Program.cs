1b67cfe8262a13edc143fede64f3ff185e5b46d7
antonio-dembo <beboantonio1@hotmail.com>
2023-03-29 11:53:07 -0300

    Add more basics filtering

--- demos/M03 Query a Database/Before/PublisherConsole/Program.cs
+++ demos/M03 Query a Database/Before/PublisherConsole/Program.cs
@@ -8,9 +8,62 @@ PubContext _context = new PubContext();
 //database created in privious module
 
 QueryFilters();
+//FindIt();
+//AddSomeMoreAuthors();
+//SkipAndTakeAuthors();
+//SortAuthors();
+
+void SortAuthors()
+{
+    var authorsByLastName = _context.Authors
+        .OrderBy(a => a.LastName)
+        .ThenBy(a => a.FirstName).ToList();
+    authorsByLastName.ForEach(a => Console.WriteLine(a.LastName + ","+ a.FirstName));
+
+    var authorsDescending = _context.Authors
+        .OrderByDescending(a => a.LastName)
+        .ThenByDescending(a => a.FirstName).ToList();
+    Console.WriteLine("**Descending Last and First**");
+    authorsDescending.ForEach(a => Console.WriteLine(a.LastName + ","+ a.FirstName));
+
+
+}
+
+void AddSomeMoreAuthors() 
+{
+    _context.Authors.Add(new Author { FirstName = "Rhoda", LastName = "Lerman" });
+    _context.Authors.Add(new Author { FirstName = "Don", LastName = "Jones" });
+    _context.Authors.Add(new Author { FirstName = "Jim", LastName = "Christopher" });
+    _context.Authors.Add(new Author { FirstName = "Stephen", LastName = "Haunts" });
+    _context.SaveChanges();
+}
+
+void SkipAndTakeAuthors()
+{
+    int groupSize = 2;
+    for (int i = 0; i < 5; i++)
+    {
+        var authors = _context.Authors.Skip(groupSize * i).Take(groupSize).ToList();
+        Console.WriteLine($"Group {i}:");
+        foreach (var author in authors)
+        {
+            Console.WriteLine($" {author.FirstName} {author.LastName}");
+        }
+    }
+}
 
 void QueryFilters()
 {
-    string name = "Josie";
-    var authors = _context.Authors.Where( s => s.FirstName == name).ToList();
+    //string name = "Josie";
+    //var authors = _context.Authors.Where( s => s.FirstName == name).ToList();
+    string filter = "L%";
+    var authors = _context.Authors
+        .Where(s => EF.Functions.Like(s.LastName, filter))
+        .OrderByDescending(a => a.FirstName).ToList();
+    authors.ForEach(a => Console.WriteLine(a.LastName +","+ a.FirstName));
+}
+
+void FindIt()
+{
+    var authorIdTwo = _context.Authors.Find(2);
 }
