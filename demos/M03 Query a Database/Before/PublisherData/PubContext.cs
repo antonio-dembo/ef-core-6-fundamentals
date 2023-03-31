5a3be4f4e67774dbdf2b07095811918529f4de7a
antonio-dembo <beboantonio1@hotmail.com>
2023-03-31 11:13:10 -0300

    adding magrations + seeding a database via migrations

--- demos/M03 Query a Database/Before/PublisherData/PubContext.cs
+++ demos/M03 Query a Database/Before/PublisherData/PubContext.cs
@@ -15,5 +15,21 @@ namespace PublisherData
             );
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName ="Rhoda", LastName = "Lerman"});
+
+            var newAuthors = new List<Author>
+            {
+                new Author { Id= 2, FirstName = "Ruth", LastName = "OZeki" },
+                new Author { Id= 3, FirstName = "Sofia", LastName = "Segovia" },
+                new Author { Id= 4, FirstName = "Ursula K.", LastName = "Howey" },
+                new Author { Id= 5, FirstName = "Hugh", LastName = "Haunts" },
+                new Author { Id= 6, FirstName = "Isabelle", LastName = "Allende" },
+            };
+
+            modelBuilder.Entity<Author>().HasData(newAuthors );
+        }
     }
 }
