f641ce170ab520da1cbb91c4bbcbff5e42c55e4f
antonio-dembo <beboantonio1@hotmail.com>
2023-04-28 15:53:32 -0300

    adding one-to-many relationship + benefiting from FK prop so seed more data

--- demos/M03 Query a Database/Before/PublisherData/PubContext.cs
+++ demos/M03 Query a Database/Before/PublisherData/PubContext.cs
@@ -18,18 +18,32 @@ namespace PublisherData
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Author>().HasData(
-                new Author { Id = 1, FirstName ="Rhoda", LastName = "Lerman"});
+                new Author { AuthorId = 1, FirstName ="Rhoda", LastName = "Lerman"});
 
-            var newAuthors = new List<Author>
+            var authorList = new List<Author>
             {
-                new Author { Id= 2, FirstName = "Ruth", LastName = "OZeki" },
-                new Author { Id= 3, FirstName = "Sofia", LastName = "Segovia" },
-                new Author { Id= 4, FirstName = "Ursula K.", LastName = "Howey" },
-                new Author { Id= 5, FirstName = "Hugh", LastName = "Haunts" },
-                new Author { Id= 6, FirstName = "Isabelle", LastName = "Allende" },
+                new Author { AuthorId= 2, FirstName = "Ruth", LastName = "OZeki" },
+                new Author { AuthorId= 3, FirstName = "Sofia", LastName = "Segovia" },
+                new Author { AuthorId= 4, FirstName = "Ursula K.", LastName = "Howey" },
+                new Author { AuthorId= 5, FirstName = "Hugh", LastName = "Haunts" },
+                new Author { AuthorId= 6, FirstName = "Isabelle", LastName = "Allende" },
             };
 
-            modelBuilder.Entity<Author>().HasData(newAuthors );
+            modelBuilder.Entity<Author>().HasData(authorList);
+
+            var someBooks = new List<Book> {
+                new Book { BookId= 1, AuthorId=1, Title = "In God's Ear", PublishDate = new DateTime(1989,3,1)},
+                new Book { BookId= 2, AuthorId=2, Title = "A Tale For the Time Being", PublishDate = new DateTime(2013,12,31)},
+                new Book { BookId= 3, AuthorId=3, Title = "The Left Hand of Darkness", PublishDate = new DateTime(1961,3,1)},
+            };
+            modelBuilder.Entity<Book>().HasData(someBooks);
+
+            //example of more advenced mapping to specify
+            //a one-to-many between author and book when
+            //there are no navigation propperties
+            //modelBuilder.Entity<Author>()
+            //    .HasMany<Book>()
+            //    .WithOne();
         }
     }
 }
